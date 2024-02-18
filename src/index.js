530bec8e6a56b459a9744afe84b956fb43619faf
revan1010 <76840558+revan1010@users.noreply.github.com>
2024-02-18 13:06:38 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import ReactDOM from "react-dom/client";
-import { RecoilRoot } from "recoil"; // Import RecoilRoot
+import { RecoilRoot } from "recoil"; 
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
-    {/* Wrap your App component with RecoilRoot */}
+
     <App />
   </RecoilRoot>
 );
